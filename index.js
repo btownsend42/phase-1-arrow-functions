7eb3d53a32e8b38ba41566b363643b2ba0dcbfd2
Brianna Townsed <b.townsend42@yahoo.com>
2023-07-02 17:57:58 -0500

    DivideSqaureAdd

---
+++ index.js
@@ -0,0 +1,21 @@
+// index.js
+
+// Function expression called divide
+const divide = function () {
+  return 2000 / 100;
+};
+
+// Arrow function called square
+const square = (number) => {
+  return number * number;
+};
+
+// Arrow function called add
+const add = (a, b) => {
+  return a + b;
+};
+
+// Example usage:
+console.log(divide()); // Output: 20
+console.log(square(5)); // Output: 25
+console.log(add(2, 3)); // Output: 5
